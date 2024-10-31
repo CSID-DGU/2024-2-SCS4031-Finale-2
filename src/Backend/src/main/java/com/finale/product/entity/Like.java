ef6f95fedaa6bc8978e0ec7f5091a0d526f4c0bc
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-01 03:43:22 +0900

    [feat] ERD 설계에 따른 Product Entity 구현

---
+++ src/Backend/src/main/java/com/finale/product/entity/Like.java
@@ -0,0 +1,24 @@
+package Backend.src.main.product.entity;
+
+import com.finale.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "likes")
+public class Like {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+}
---
+++ src/Backend/src/main/java/com/finale/product/entity/OrderStatus.java
@@ -0,0 +1,5 @@
+package Backend.src.main.java.com.finale.product.entity;
+
+public enum OrderStatus {
+	ORDER, CANCEL
+}
