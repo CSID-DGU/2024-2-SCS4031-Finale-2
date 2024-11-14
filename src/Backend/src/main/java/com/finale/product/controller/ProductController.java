13d672d14d02081456c97f9c0325ff8029159737
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-14 19:11:40 +0900

    [feat] product 개별 상세 조회 기능

--- src/Backend/src/main/java/com/finale/product/controller/ProductController.java
+++ src/Backend/src/main/java/com/finale/product/controller/ProductController.java
@@ -1,13 +1,11 @@
 package com.finale.product.controller;
 
+import com.finale.product.dto.ProductResponse;
 import com.finale.product.dto.ProductSaveRequest;
 import com.finale.product.entity.Product;
 import com.finale.product.service.ProductService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -18,10 +16,16 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
-    
+
     @PostMapping
     public ResponseEntity<Product> saveProduct(@RequestBody ProductSaveRequest productSaveRequest) {
         Product product = productService.save(productSaveRequest);
         return ResponseEntity.ok().body(product);
+    }
+    
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductResponse> getProductInfo(@PathVariable("productId") Long productId) {
+        Product product = productService.find(productId);
+        return ResponseEntity.ok(ProductResponse.from(product));
     }
 }
--- src/Backend/src/main/java/com/finale/product/service/ProductService.java
+++ src/Backend/src/main/java/com/finale/product/service/ProductService.java
@@ -12,11 +12,15 @@ public class ProductService {
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
-    
+
     public Product save(ProductSaveRequest productSaveRequest) {
         //TODO ArtistInfo 코드 병합시 수정 예정
         Product product = productSaveRequest.toEntity(null);
         productRepository.save(product);
         return product;
+    }
+    
+    public Product find(Long productId) {
+        return productRepository.findById(productId).orElseThrow(() -> new IllegalArgumentException("유효하지 않은 id입니다"));
     }
 }
