8752374a5c6aaa62d93951e055d6e7009fd6bf5c
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-18 19:51:52 +0900

    [ADD] JpaAuditing을 이용한 BaseTimeEntity 작성

--- src/Backend/src/main/java/com/finale/Application.java
+++ src/Backend/src/main/java/com/finale/Application.java
@@ -1,7 +1,10 @@
 package com.finale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
