ef587f6108ef76746ff0fff675a0a4b191553215
Yeonming <2019110914@dongguk.edu>
2024-11-18 15:35:08 +0900

    Add repository

---
+++ src/Backend/src/main/java/com/finale/user/repository/ArtistInfoRepository.java
@@ -0,0 +1,7 @@
+package com.finale.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.helpmeCookies.user.entity.ArtistInfo;
+
+public interface ArtistInfoRepository extends JpaRepository<ArtistInfo, Long> {
+}
