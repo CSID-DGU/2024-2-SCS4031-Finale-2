1635a0c54df4476143966567196f4ac4a0ad7f47
Yeonming <2019110914@dongguk.edu>
2024-11-19 13:41:29 +0900

    [feat]Add EntityListeners

--- src/Backend/src/main/java/com/finale/user/entity/User.java
+++ src/Backend/src/main/java/com/finale/user/entity/User.java
@@ -4,12 +4,14 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 import com.finale.product.entity.HashTag;
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -27,6 +29,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "users") // 예약어 회피
 @AllArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
