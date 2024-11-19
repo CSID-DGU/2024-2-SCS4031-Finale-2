c206b3a939c8b9dde0492593e99521042acecb01
Yeonming <2019110914@dongguk.edu>
2024-11-19 13:35:00 +0900

    [feat] Add findById

--- src/Backend/src/main/java/com/finale/user/repository/UserRepository.java
+++ src/Backend/src/main/java/com/finale/user/repository/UserRepository.java
@@ -1,6 +1,8 @@
 
 package com.finale.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.finale.user.entity.User;
@@ -8,4 +10,5 @@ import com.finale.user.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    import java.util.Optional;
 }
