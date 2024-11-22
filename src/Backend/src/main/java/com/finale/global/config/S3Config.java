03a678050c9f3ea03e9ac470b6893c72ac324113
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-23 01:42:36 +0900

    S3Config 파일 Bean 어노테이션 설정

--- src/Backend/src/main/java/com/finale/global/config/S3Config.java
+++ src/Backend/src/main/java/com/finale/global/config/S3Config.java
@@ -7,6 +7,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -17,8 +18,11 @@ public class S3Config {
     private String secretKey;
     @Value("${cloud.aws.region.static}")
     private String region;
+
+    @Bean
     public AmazonS3 amazonS3() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey,secretKey);
+
         return AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
