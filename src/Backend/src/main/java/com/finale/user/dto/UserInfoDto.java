60205d10b294e58525aefabe7c6f4a7ae7570113
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-23 17:01:06 +0900

    refact userDomain

--- src/Backend/src/main/java/com/finale/user/dto/UserInfoDto.java
+++ src/Backend/src/main/java/com/finale/user/dto/UserInfoDto.java
@@ -8,8 +8,6 @@ import com.finale.user.entity.UserInfo;
 
 public record UserInfoDto(
 	String name,
-	String userImageUrl,
-	String nickname,
 	String email,
 	String birthdate,
 	String phone,
@@ -19,8 +17,6 @@ public record UserInfoDto(
 	public static UserInfoDto fromEntity(UserInfo userInfo) {
 		return new UserInfoDto(
 			userInfo.getName(),
-			userInfo.getUserImageUrl(),
-			userInfo.getNickname(),
 			userInfo.getEmail(),
 			userInfo.getBirthdate(),
 			userInfo.getPhone(),
@@ -28,12 +24,10 @@ public record UserInfoDto(
 			userInfo.getHashTags()
 		);
 	}
-    
+
 	public UserInfo toEntity() {
 		return new UserInfo(
 			name,
-			userImageUrl,
-			nickname,
 			email,
 			birthdate,
 			phone,
