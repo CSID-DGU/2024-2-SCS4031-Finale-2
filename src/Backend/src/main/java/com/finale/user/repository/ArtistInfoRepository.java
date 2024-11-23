ae4b2e9c9f73848e2f8d0c4bf322a14ed4399a57
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-23 15:09:07 +0900

    add ArtistService

--- src/Backend/src/main/java/com/finale/user/repository/ArtistInfoRepository.java
+++ src/Backend/src/main/java/com/finale/user/repository/ArtistInfoRepository.java
@@ -1,7 +1,12 @@
 package com.finale.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.finale.user.entity.ArtistInfo;
 
 public interface ArtistInfoRepository extends JpaRepository<ArtistInfo, Long> {
+	Optional<ArtistInfo> findByUserId(Long userId);
+	boolean existsByUserId(Long userId);
 }
--- src/Backend/src/main/java/com/finale/user/repository/BusinessArtistRepository.java
+++ src/Backend/src/main/java/com/finale/user/repository/BusinessArtistRepository.java
@@ -1,7 +1,12 @@
 package com.finale.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finale.user.entity.ArtistInfo;
 import com.finale.user.entity.BusinessArtist;
 
 public interface BusinessArtistRepository extends JpaRepository<BusinessArtist, Long> {
+	Optional<BusinessArtist> findByArtistInfo(ArtistInfo artistInfo);
 }
