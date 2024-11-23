aedd7f4a5c1f55565b20167f757d73c1c0e80a28
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-23 15:13:26 +0900

    add ArtistService

--- src/Backend/src/main/java/com/finale/user/dto/request/StudentArtistReq.java
+++ src/Backend/src/main/java/com/finale/user/dto/request/StudentArtistReq.java
@@ -1,8 +1,11 @@
 package com.finale.user.dto.request;
+
 import com.finale.user.dto.StudentArtistDto;
+
 public record StudentArtistReq(
 	String schoolEmail,
 	String schoolName,
-	String major
+	String major,
+	String about
 ) {
 }
--- src/Backend/src/main/java/com/finale/user/repository/SocialRepository.java
+++ src/Backend/src/main/java/com/finale/user/repository/SocialRepository.java
@@ -1,7 +1,18 @@
 package com.finale.user.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.finale.user.entity.Social;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.finale.user.entity.ArtistInfo;
+import com.finale.user.entity.Social;
+import com.finale.user.entity.User;
+
+@Repository
 public interface SocialRepository extends JpaRepository<Social, Long> {
+	Boolean existsByFollowerAndFollowing(User follower, ArtistInfo following);
+	Optional<Social> findByFollowerAndFollowing(User follower, ArtistInfo following);
 }
