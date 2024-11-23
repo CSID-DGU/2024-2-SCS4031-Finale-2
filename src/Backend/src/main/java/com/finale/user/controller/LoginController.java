0cf92daca952d0f2dede0eb8e56d7e8a274219b7
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-23 15:01:14 +0900

    refact testController

--- src/Backend/src/main/java/com/finale/user/controller/LoginController.java
+++ src/Backend/src/main/java/com/finale/user/controller/LoginController.java
@@ -13,6 +13,7 @@ import com.finale.global.jwt.JwtToken;
 import com.finale.global.jwt.JwtUser;
 import com.finale.product.entity.HashTag;
 import com.finale.user.entity.User;
+import com.finale.user.entity.UserInfo;
 import com.finale.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -27,22 +28,29 @@ public class LoginController {
 	// 임시 회원가입 url. 유저를 생성하고 jwt 토큰을 반환한다.
 	@GetMapping("/test/signup")
 	public JwtToken signup() {
-		User user = User.builder()
-			.nickname("test")
+		UserInfo userInfo = UserInfo.builder()
+			.userImageUrl("https://www.naver.com")
 			.email("test@test")
-			.birthdate("1999-01-01")
-			.address("서울시 강남구")
+			.birthdate("1995-01-01")
 			.phone("010-1234-5678")
-			.hashTags(List.of(HashTag.DREAMLIKE, HashTag.VIBRANCE))
+			.hashTags(List.of(HashTag.DREAMLIKE))
+			.name("test")
+			.nickname("test")
+			.address("서울시 강남구")
 			.build();
+
+		User user = User.builder()
+			.userInfo(userInfo)
+			.build();
+			
 		userRepository.save(user);
 		return jwtProvider.createToken(JwtUser.of(user.getId()));
 	}
-    
+
 	// 임시 로그인 url. 로그인한 유저의 정보의 일부를 반환한다.
 	@GetMapping("/login")
 	public String login(@AuthenticationPrincipal JwtUser jwtUser) {
 		User user = userRepository.findById(jwtUser.getId()).orElseThrow();
-		return user.getEmail();
+		return user.getUserInfo().getEmail();
 	}
 }
