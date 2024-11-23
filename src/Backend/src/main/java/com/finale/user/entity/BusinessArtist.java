7927f37d5d41ff7d0ae52e6fc9357ef3b1b74347
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-23 16:26:21 +0900

    Add Cascade

--- src/Backend/src/main/java/com/finale/user/entity/BusinessArtist.java
+++ src/Backend/src/main/java/com/finale/user/entity/BusinessArtist.java
@@ -1,10 +1,12 @@
 package com.finale.user.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -12,6 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -31,6 +34,7 @@ public class BusinessArtist {
 	@Column(nullable = false)
 	private String headName;
 
-	@OneToOne
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "artist_info_id")
 	private ArtistInfo artistInfo;
 }
