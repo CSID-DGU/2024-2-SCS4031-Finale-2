5765ff3cb7d15716906249794e48a1cfefda0f80
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-23 16:41:11 +0900

    add swagger Operation

--- src/Backend/src/main/java/com/finale/user/controller/UserController.java
+++ src/Backend/src/main/java/com/finale/user/controller/UserController.java
@@ -23,22 +23,27 @@ import com.finale.user.dto.UserTypeDto;
 import com.finale.user.dto.response.UserFollowingRes;
 import com.finale.user.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "유저 및 팔로우 기능", description = "유저 및 팔로우 기능과 관련된 API")
 public class UserController {
 	private final UserService userService;
 
-	// 유저 일반 정보 조회
+
+	@Operation(summary = "유저 일반 정보 조회", description = "로그인한 유저의 username, imageUrl, hashtag를 조회한다.")
 	@GetMapping("/v1/users")
 	public UserCommonInfoRes getUsers(
 		@AuthenticationPrincipal JwtUser jwtUser
 	) {
 		return UserCommonInfoRes.fromDto(userService.getUserInfo(jwtUser.getId()));
 	}
-	
+
 	// 유저 상세 정보 조회
+	@Operation(summary = "유저 상세 정보 조회", description = "로그인한 유저의 이름, 주소를 비롯한 개인정보를 함께 조회한다.")
 	@GetMapping("/v1/users/details")
 	public UserDetailsInfoRes getUserDetails(
 		@AuthenticationPrincipal JwtUser jwtUser
@@ -47,6 +52,7 @@ public class UserController {
 	}
 
 	// 유저 타입 조회
+	@Operation(summary = "유저 타입 조회", description = "로그인한 유저의 타입과 권한을 조회한다.")
 	@GetMapping("/v1/users/type")
 	public UserTypeDto getUserType(
 		@AuthenticationPrincipal JwtUser jwtUser
@@ -55,6 +61,7 @@ public class UserController {
 	}
 
 	// 유저 정보 수정
+	@Operation(summary = "유저 정보 수정", description = "로그인한 유저의 개인정보를 수정한다.")
 	@PutMapping("/v1/users")
 	public String updateUserInfo(
 		@AuthenticationPrincipal JwtUser jwtUser,
@@ -65,7 +72,7 @@ public class UserController {
 		return "ok";
 	}
 
-	// 아티스트 팔로우하기
+	@Operation(summary = "아티스트 팔로우하기", description = "로그인한 유저가 특정 아티스트를 팔로우한다.")
 	@PostMapping("/v1/users/following/{artistId}")
 	public String followArtist(
 		@AuthenticationPrincipal JwtUser jwtUser,
@@ -75,7 +82,7 @@ public class UserController {
 		return "ok";
 	}
 
-	// 아티스트 팔로우 취소하기
+	@Operation(summary = "아티스트 팔로우 취소하기", description = "로그인한 유저가 특정 아티스트를 팔로우 취소한다.")
 	@DeleteMapping("/v1/users/following/{artistId}")
 	public String unfollowArtist(
 		@AuthenticationPrincipal JwtUser jwtUser,
@@ -85,7 +92,7 @@ public class UserController {
 		return "ok";
 	}
 
-	// 유저 팔로우 목록 조회
+	@Operation(summary = "팔로잉 목록 조회", description = "로그인한 유저의 팔로우한 아티스트 목록을 조회한다.")
 	@GetMapping("/v1/users/following")
 	public Page<UserFollowingRes> getFollowingList(
 		@AuthenticationPrincipal JwtUser jwtUser,
@@ -95,6 +102,7 @@ public class UserController {
 	}
 
 	// 유저 탈퇴
+	@Operation(summary = "유저 탈퇴", description = "로그인한 유저의 정보를 삭제한다.")
 	@DeleteMapping("/v1/users")
 	public String deleteUser(
 		@AuthenticationPrincipal JwtUser jwtUser
