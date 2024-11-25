efe73420c06e969c81549fd122701292683abeed
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-25 18:22:30 +0900

    feat(user.controller): 작가 검색 기능 작성

--- src/Backend/src/main/java/com/finale/user/controller/ArtistController.java
+++ src/Backend/src/main/java/com/finale/user/controller/ArtistController.java
@@ -1,24 +1,24 @@
 package com.finale.user.controller;
 
+import com.finale.global.jwt.JwtUser;
+import com.finale.user.controller.apiDocs.ArtistApiDocs;
+import com.finale.user.dto.ArtistInfoPage;
+import com.finale.user.dto.request.BusinessArtistReq;
+import com.finale.user.dto.request.StudentArtistReq;
+import com.finale.user.dto.response.ArtistDetailsRes;
+import com.finale.user.service.ArtistService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.finale.global.jwt.JwtProvider;
-import com.finale.global.jwt.JwtUser;
-import com.finale.user.controller.apiDocs.ArtistApiDocs;
-import com.finale.user.dto.request.BusinessArtistReq;
-import com.finale.user.dto.request.StudentArtistReq;
-import com.finale.user.dto.response.ArtistDetailsRes;
-import com.finale.user.service.ArtistService;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -62,5 +62,14 @@ public class ArtistController implements ArtistApiDocs {
 		@AuthenticationPrincipal JwtUser jwtUser
 	) {
 		return artistService.getArtistDetails(jwtUser.getId());
+	}
+	@GetMapping("/v1/artists")
+	public ResponseEntity<ArtistInfoPage.Paging> getArtistsByPage(
+		@RequestParam("query") String query,
+		@RequestParam(name = "size", required = false, defaultValue = "20") int size,
+		@RequestParam("page") int page
+	) {
+		var pageable = PageRequest.of(page, size);
+		return ResponseEntity.ok(artistService.getArtistsByPage(query, pageable));
 	}
 }
