25ba28e8493a78f6e31d19822baf7b0dbb0caf0d
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-25 17:50:46 +0900

    [fix] 상품 정렬 변환 유틸 메서드 스네이크 케이스로 변경

--- src/Backend/src/main/java/com/finale/product/util/SortUtil.java
+++ src/Backend/src/main/java/com/finale/product/util/SortUtil.java
@@ -3,7 +3,7 @@ import org.springframework.data.domain.Sort;
 public class SortUtil {
     public static Sort convertProductSort(ProductSort productSort) {
         return switch (productSort) {
-            case LATEST -> Sort.by(Sort.Order.desc("createdDate"));
+            case LATEST -> Sort.by(Sort.Order.desc("created_date"));
             default -> Sort.unsorted();
         };
     }
