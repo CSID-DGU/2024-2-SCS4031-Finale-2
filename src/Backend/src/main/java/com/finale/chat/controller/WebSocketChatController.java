b7a49026219aa632929a43a99e2c247e74e7bd82
Yeonming <2019110914@dongguk.edu>
2024-11-25 19:36:46 +0900

    [feat] chat 기능

---
+++ src/Backend/src/main/java/com/finale/chat/controller/WebSocketChatController.java
@@ -0,0 +1,85 @@
+package com.finale.chat.controller;
+
+
+import com.finale.chat.dto.ChatMessageDto;
+import com.finale.chat.entity.ChatMessage;
+import com.finale.chat.entity.ChatRoom;
+import com.finale.chat.entity.MessageType;
+import com.finale.chat.service.ChatMessageService;
+import com.finale.chat.service.ChatRoomService;
+import com.finale.global.exception.user.UserNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+
+@Slf4j
+@RestController
+public class WebSocketChatController {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+    private final ChatMessageService chatMessageService;
+    private final ChatRoomService chatRoomService;
+
+
+    public WebSocketChatController(SimpMessageSendingOperations messagingTemplate,
+                                   ChatMessageService chatMessageService,
+                                   ChatRoomService chatRoomService) {
+        this.messagingTemplate = messagingTemplate;
+        this.chatMessageService = chatMessageService;
+        this.chatRoomService = chatRoomService;
+
+    }
+
+    @MessageMapping("/chat/{chatRoomId}")
+    public void chat(@DestinationVariable Long chatRoomId, @RequestBody ChatMessageDto messageDto) {
+        ChatMessage savedMessage = chatMessageService.saveMessage(chatRoomId, messageDto);
+
+        messagingTemplate.convertAndSend("/v1/sub/chat/rooms/" + chatRoomId, savedMessage);
+    }
+
+    @MessageMapping("/chat/{chatRoomId}/file")
+    @Operation(summary = "파일 전송", description = "WebSocket을 통해 특정 채팅방에 파일을 전송합니다.")
+    public void sendFile(@DestinationVariable Long chatRoomId,
+                         @RequestParam String fileBase64,
+                         @RequestParam String userEmail) {
+        try {
+            ChatMessage message = chatMessageService.saveFileMessage(chatRoomId, userEmail, fileBase64);
+
+            byte[] imageBytes = chatMessageService.convertImageUrlToBytes(message.getContent());
+            String encodedContent = ChatMessageDto.getImageContent(imageBytes);
+
+            ChatMessageDto chatMessageDto = new ChatMessageDto(
+                    message.getChatRoom().getId(),
+                    message.getSender().getEmail(),
+                    encodedContent,
+                    message.getTimestamp().toString(),
+                    MessageType.IMAGE
+            );
+
+            messagingTemplate.convertAndSend("/api/sub/chat/rooms/" + chatRoomId, chatMessageDto);
+        } catch (UserNotFoundException | IOException e) {
+
+            log.error("파일 전송 중 오류 발생: " + e.getMessage(), e);
+        }
+    }
+
+
+    @SubscribeMapping("/chat/rooms/{chatRoomId}/list")
+    public List<ChatMessage> sendInitialMessages(@DestinationVariable Long chatRoomId) {
+        ChatRoom chatRoom = chatRoomService.getChatRoomById(chatRoomId);
+
+        return chatMessageService.getMessagesByChatRoom(chatRoom);
+    }
+
+
+}
---
+++ src/Backend/src/main/java/com/finale/chat/util/ImageStorageUtil.java
@@ -0,0 +1,118 @@
+package com.finale.chat.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Base64;
+
+@Component
+public class ImageStorageUtil {
+    private static final Logger logger = LoggerFactory.getLogger(ImageStorageUtil.class);
+    private static final String STORAGE_DIR = "src/main/resources/imageStorage/"; // 실제 저장 위치를 지정합니다.
+
+    public static String saveImage(MultipartFile imageFile) throws IOException {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS"));
+        String imageName = "product" + "_" + timestamp + ".jpg";
+
+        // ClassPathResource를 사용하여 리소스 경로를 얻습니다.
+        ClassPathResource resource = new ClassPathResource(STORAGE_DIR);
+
+        // 실제 파일 저장 경로를 지정합니다.
+        File storageDir = new File(resource.getPath());
+
+        if (!storageDir.exists()) {
+            storageDir.mkdirs(); // 디렉토리가 존재하지 않으면 생성합니다.
+        }
+
+        File outputFile = new File(storageDir, imageName);
+        try (FileOutputStream fos = new FileOutputStream(outputFile)) {
+            fos.write(imageFile.getBytes());
+        }
+
+        String filePath = outputFile.getAbsolutePath();
+        logger.info("Image saved successfully at path: {}", filePath);
+        return filePath;
+    }
+
+    // URL-safe Base64 Encoder 사용
+    public static String encodeImagePathToBase64(String imagePath) {
+        return Base64.getUrlEncoder().encodeToString(imagePath.getBytes());
+    }
+
+    public static String decodeBase64ImagePath(String base64ImagePath) {
+        byte[] decodedBytes = Base64.getUrlDecoder().decode(base64ImagePath);
+        return new String(decodedBytes);
+    }
+
+    public static void deleteImage(String imagePath) {
+        File imageFile = new File(imagePath);
+        logger.info("Attempting to delete image at path: {}", imagePath);
+        if (imageFile.exists()) {
+            imageFile.delete();
+            logger.info("Image deleted successfully at path: {}", imagePath);
+        }
+    }
+
+    //chat
+    // 채팅 이미지 저장 메서드
+    public String saveChatImage(MultipartFile imageFile, String folderPath) throws IOException {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS"));
+        String imageName = "image_" + timestamp + ".jpg";
+
+        File storageDir = new File(STORAGE_DIR + folderPath);
+        logger.info("Attempting to save image at path: {}", storageDir.getAbsolutePath());
+        if (!storageDir.exists()) {
+            storageDir.mkdirs();
+        }
+
+        File outputFile = new File(storageDir, imageName);
+        try (FileOutputStream fos = new FileOutputStream(outputFile)) {
+            fos.write(imageFile.getBytes());
+        }
+
+        String filePath = outputFile.getAbsolutePath();
+        logger.info("Chat image saved successfully at path: {}", filePath);
+        return filePath;
+    }
+
+    // Base64 데이터를 이미지 파일로 저장하는 공통 메서드
+    public String saveBase64Image(String fileBase64, String folderPath) throws IOException {
+        byte[] decodedBytes = Base64.getDecoder().decode(fileBase64);
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS"));
+        String imageName = "image_" + timestamp + ".jpg";
+
+        File storageDir = new File(STORAGE_DIR + folderPath);
+        if (!storageDir.exists()) {
+            storageDir.mkdirs();
+        }
+
+        File outputFile = new File(storageDir, imageName);
+        try (FileOutputStream fos = new FileOutputStream(outputFile)) {
+            fos.write(decodedBytes);
+        }
+
+        return outputFile.getAbsolutePath();  // 파일 경로 반환
+    }
+
+    // 채팅방 이미지 폴더 삭제 메서드
+    public void deleteChatFolder(String folderPath) {
+        File folder = new File(STORAGE_DIR + folderPath);
+        if (folder.exists()) {
+            for (File file : folder.listFiles()) {
+                file.delete();
+            }
+            folder.delete();
+            logger.info("Chat folder deleted successfully: {}", folderPath);
+        }
+    }
+
+}
+
