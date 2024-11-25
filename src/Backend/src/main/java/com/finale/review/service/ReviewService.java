69e355f7061e45ed9c08dd5eafdcda07836995a9
Yeonming <2019110914@dongguk.edu>
2024-11-25 16:59:24 +0900

    [fix] 감상평 수정 기능

--- src/Backend/src/main/java/com/finale/review/service/ReviewService.java
+++ src/Backend/src/main/java/com/finale/review/service/ReviewService.java
@@ -2,6 +2,7 @@ package com.finale.review.service;
 import com.finale.product.entity.Product;
 import com.finale.product.repository.ProductRepository;
 import com.finale.review.dto.ReviewRequest;
+import com.finale.review.entity.Review;
 import com.finale.review.repository.ReviewRepository;
 import com.finale.user.entity.User;
 import com.finale.user.repository.UserRepository;
@@ -18,5 +19,9 @@ public class ReviewService {
         User writer = userRepository.findById(request.writerId()).orElseThrow(() -> new IllegalArgumentException("유효하지 않은 writerID입니다."));
         Product product = productRepository.findById(productId).orElseThrow(() -> new IllegalArgumentException("유효하지 않은 productID입니다."));
         reviewRepository.save(request.toEntity(writer,product));
+    }
+    public void editReview(ReviewRequest request, Long productId, Long reviewId) {
+        Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new IllegalArgumentException("유효하지 않은 reviewId 입니다."));
+        review.updateContent(request.content());
     }
  }
