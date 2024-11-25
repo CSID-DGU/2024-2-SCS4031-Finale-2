5bedac74fe00f6ad88a741c0d65a84caa3482cde
Yeonming <2019110914@dongguk.edu>
2024-11-25 15:53:12 +0900

    [fix] 상품 등록 수정 시 artistInfo 관련 코드 보완

---
+++ src/Backend/src/main/java/com/finale/product/dto/ProductRequest.java
@@ -0,0 +1,33 @@
+package com.finale.product.dto;
+
+import com.finale.product.entity.Category;
+import com.finale.product.entity.HashTag;
+import com.finale.product.entity.Product;
+import com.finale.user.entity.ArtistInfo;
+
+import java.util.List;
+
+public record ProductRequest(
+        String name,
+        String category,
+        String size,
+        Long price,
+        String description,
+        String preferredLocation,
+        List<HashTag> hashTags,
+        Long artistInfoId,
+        List<ImageUpload> productImages
+) {
+    public Product toEntity(ArtistInfo artistInfo) {
+        return Product.builder()
+                .name(name)
+                .category(Category.fromString(category))
+                .size(size)
+                .price(price)
+                .description(description)
+                .preferredLocation(preferredLocation)
+                .hashTags(hashTags)
+                .artistInfo(artistInfo)
+                .build();
+    }
+}
---
+++ src/Backend/src/main/java/com/finale/product/dto/ProductResponse.java
@@ -0,0 +1,36 @@
+package com.finale.product.dto;
+
+import com.finale.product.entity.HashTag;
+import com.finale.product.entity.Product;
+
+import java.util.List;
+
+public record ProductResponse(
+        Long id,
+        String name,
+        String category,
+        String size,
+        Long price,
+        String description,
+        String preferredLocation,
+        List<HashTag> hashTags,
+        ArtistInfo artistInfo
+) {
+    public record ArtistInfo(Long artistId, String artistName) {
+    }
+
+    public static ProductResponse from(Product product) {
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getCategory().getName(),
+                product.getSize(),
+                product.getPrice(),
+                product.getDescription(),
+                product.getPreferredLocation(),
+                product.getHashTags(),
+
+                new ArtistInfo(product.getArtistInfo().getId(),product.getArtistInfo().getNickname())
+                );
+            }
+        }
