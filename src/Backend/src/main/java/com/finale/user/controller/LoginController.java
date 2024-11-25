7bc34599941ae6c9411aacd272a074daeb093db5
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-25 22:16:32 +0900

    refact Controller

--- src/Backend/src/main/java/com/finale/user/controller/LoginController.java
+++ src/Backend/src/main/java/com/finale/user/controller/LoginController.java
@@ -1,20 +1,27 @@
 package com.finale.user.controller;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.finale.global.jwt.JwtProvider;
 import com.finale.global.jwt.JwtToken;
 import com.finale.global.jwt.JwtUser;
+import com.finale.global.security.UserDetailService;
 import com.finale.product.entity.HashTag;
+import com.finale.user.dto.KakaoOAuth2Response;
 import com.finale.user.entity.User;
 import com.finale.user.entity.UserInfo;
 import com.finale.user.repository.UserRepository;
+import com.finale.user.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,36 +29,38 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 //Todo: Swagger 추가
 public class LoginController {
-	private final UserRepository userRepository;
+	private final UserDetailService userDetailsService;
+	private final UserService userService;
 	private final JwtProvider jwtProvider;
 
 	// 임시 회원가입 url. 유저를 생성하고 jwt 토큰을 반환한다.
 	@GetMapping("/test/signup")
 	public JwtToken signup() {
-		UserInfo userInfo = UserInfo.builder()
-			
-			.email("test@test")
-			.birthdate("1995-01-01")
-			.phone("010-1234-5678")
-			.hashTags(List.of(HashTag.DREAMLIKE))
-			.name("test")
-			.address("서울시 강남구")
-			.build();
 
-		User user = User.builder()
-			.userInfo(userInfo)
-			.nickname("test")
-			.userImageUrl("test")
-			.build();
-			
-		userRepository.save(user);
-		return jwtProvider.createToken(JwtUser.of(user.getId()));
+		return jwtProvider.createToken(JwtUser.of(userService.getUserInfo(1L).id()));
 	}
 
-	// 임시 로그인 url. 로그인한 유저의 정보의 일부를 반환한다.
-	@GetMapping("/login")
-	public String login(@AuthenticationPrincipal JwtUser jwtUser) {
-		User user = userRepository.findById(jwtUser.getId()).orElseThrow();
-		return user.getUserInfo().getEmail();
+	/*
+	http://1.618.s3-website.ap-northeast-2.amazonaws.com?accessToken="asfas"&refreshToken="asdfasdf"
+또는
+http://1.618.s3-website.ap-northeast-2.amazonaws.com/signup?accessToken="asfas"&refreshToken="asdfasdf"
+로 리다이렉트
+	 */
+	@GetMapping("/oauth2/login/kakao")
+	public RedirectView loginKakao(@AuthenticationPrincipal OAuth2User oAuth2User) {
+		KakaoOAuth2Response kakaoUser = KakaoOAuth2Response.from(oAuth2User.getAttributes());
+		JwtToken jwtToken = jwtProvider.createToken(userDetailsService.loadUserByEmail(kakaoUser.email(), kakaoUser.nickname()));
+		boolean isSignup = userService.checkSignup(kakaoUser.email());
+
+		String targetUrl;
+		if (isSignup) {
+			targetUrl = "http://1.618.s3-website.ap-northeast-2.amazonaws.com?accessToken=" + jwtToken.getAccessToken() + "&refreshToken=" + jwtToken.getRefreshToken();
+		} else {
+			targetUrl = "http://1.618.s3-website.ap-northeast-2.amazonaws.com/signup?accessToken=" + jwtToken.getAccessToken() + "&refreshToken=" + jwtToken.getRefreshToken();
+		}
+
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl(targetUrl);
+		return redirectView;
 	}
 }
--- src/Backend/src/main/java/com/finale/user/controller/apiDocs/ArtistApiDocs.java
+++ src/Backend/src/main/java/com/finale/user/controller/apiDocs/ArtistApiDocs.java
@@ -1,5 +1,14 @@
 package com.finale.user.controller.apiDocs;
 
+import com.finale.global.ApiResponse.ApiResponse;
+import com.finale.global.jwt.JwtUser;
+import com.finale.user.dto.ArtistInfoPage;
+import com.finale.user.dto.request.BusinessArtistReq;
+import com.finale.user.dto.request.StudentArtistReq;
+import com.finale.user.dto.response.ArtistDetailsRes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,41 +16,41 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.finale.global.jwt.JwtUser;
-import com.finale.user.dto.request.BusinessArtistReq;
-import com.finale.user.dto.request.StudentArtistReq;
-import com.finale.user.dto.response.ArtistDetailsRes;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
-@Tag(name = "작가 관련 기능", description = "작가 관련 API")
+@Tag(name = "작가 관련 기능", description = "작가 관련 API, 작가 프로필 조회 API를 제외한 모든 API는 인증 이후 사용할 수 있습니다.(Authorization: Bearer {token}이 필요합니다.)")
 public interface ArtistApiDocs {
-	
-    @Operation(summary = "학생 작가 등록", description = "학생 작가 등록")
+
+	@Operation(summary = "학생 작가 등록", description = "학생 작가 등록")
 	@PostMapping("/v1/artists/students")
-	ResponseEntity<String> registerStudents(
+	ResponseEntity<ApiResponse<Void>> registerStudents(
 		@RequestBody StudentArtistReq artistDetailsReq,
 		@AuthenticationPrincipal JwtUser jwtUser
 	);
-	
-    @Operation(summary = "사업자 작가 등록", description = "사업자 작가 등록")
+
+	@Operation(summary = "사업자 작가 등록", description = "사업자 작가 등록")
 	@PostMapping("/v1/artists/bussinesses")
-	ResponseEntity<String> registerbussinsess(
+	ResponseEntity<ApiResponse<Void>> registerbussinsess(
 		@RequestBody BusinessArtistReq businessArtistReq,
 		@AuthenticationPrincipal JwtUser jwtUser
 	);
-	
-    @Operation(summary = "작가 프로필 조회", description = "작가 프로필 조회")
-	@GetMapping("/v1/artists/{userId}")
-	ArtistDetailsRes getArtist(
-		@AuthenticationPrincipal JwtUser jwtUser,
-		@PathVariable Long userId
-	);
-	
-    @Operation(summary = "작가 프로필 조회", description = "자신의 작가 프로필 조회")
-	@GetMapping("/v1/artist")
-	ArtistDetailsRes getArtist(
+
+	@Operation(summary = "작가 프로필 조회", description = "작가 프로필 조회")
+	@GetMapping("/v1/artists/{artistInfoId}")
+	public ResponseEntity<ApiResponse<ArtistDetailsRes>> getArtistPublicDetails(
+		@PathVariable Long artistInfoId,
 		@AuthenticationPrincipal JwtUser jwtUser
+	);
+
+	@Operation(summary = "작가 자신의 프로필 조회", description = "작가 자신의 프로필 조회")
+	@GetMapping("/v1/artist")
+	ResponseEntity<ApiResponse<ArtistDetailsRes>> getArtist(
+		@AuthenticationPrincipal JwtUser jwtUser
+	);
+
+	@Operation(summary = "작가 검색")
+	ResponseEntity<ApiResponse<ArtistInfoPage.Paging>> getArtistsByPage(
+		String query,
+		@Parameter(description = "default value 20") int size,
+		int page,
+		@Parameter(hidden = true) JwtUser jwtUser
 	);
 }
