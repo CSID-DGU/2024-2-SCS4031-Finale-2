922180bc46069b6535e75ea2694c43d5fa2005b1
Yeonming <2019110914@dongguk.edu>
2024-11-25 22:48:49 +0900

    repository 수정

--- src/Backend/src/main/java/com/finale/user/repository/SocialRepository.java
+++ src/Backend/src/main/java/com/finale/user/repository/SocialRepository.java
@@ -1,18 +1,20 @@
 package com.finale.user.repository;
 
-import java.util.Optional;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.finale.user.entity.ArtistInfo;
 import com.finale.user.entity.Social;
 import com.finale.user.entity.User;
+import java.util.Optional;
+import java.util.Set;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SocialRepository extends JpaRepository<Social, Long> {
 	Boolean existsByFollowerAndFollowing(User follower, ArtistInfo following);
+	Boolean existsByFollowerIdAndFollowingId(Long followerId, Long followingId);
 	Optional<Social> findByFollowerAndFollowing(User follower, ArtistInfo following);
+
+	@Query("SELECT s.following.id FROM Social s WHERE s.follower.id = :userId")
+	Set<Long> findFollowingIdByFollowerId(Long userId);
 }
