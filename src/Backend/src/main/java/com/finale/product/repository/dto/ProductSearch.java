887934b0c3cb260f9b80d4bd65ca82604b55e23b
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-25 18:15:25 +0900

    상품 검색 thumbnailUrl 추가

--- src/Backend/src/main/java/com/finale/product/repository/dto/ProductSearch.java
+++ src/Backend/src/main/java/com/finale/product/repository/dto/ProductSearch.java
@@ -3,7 +3,7 @@ package com.finale.product.repository.dto;
 public interface ProductSearch {
     Long getId();
     String getName();
+    String getThumbnailUrl();
     String getArtist();
     Long getPrice();
-    String getThumbnailUrl();
 }
