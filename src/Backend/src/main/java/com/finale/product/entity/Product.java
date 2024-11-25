1a798ced39e819cc985bcfcf63870237e638fd58
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-25 18:09:31 +0900

    Product thumbnailUrl 삽입 로직 작성

--- src/Backend/src/main/java/com/finale/product/entity/Product.java
+++ src/Backend/src/main/java/com/finale/product/entity/Product.java
@@ -36,6 +36,8 @@ public class Product extends BaseTimeEntity {
 	@Column(nullable = false)
 	private String preferredLocation;
 
+	private String thumbnailUrl;
+
 	@ElementCollection(targetClass = HashTag.class)
 	@CollectionTable(name = "product_hashtags")
 	@Enumerated(EnumType.STRING)
@@ -95,5 +97,9 @@ public class Product extends BaseTimeEntity {
 		this.preferredLocation = preferredLocation;
 		this.hashTags = hashTags;
 		this.artistInfo = artistInfo;
+	}
+	
+	public void updateThumbnail(String thumbnailUrl) {
+		this.thumbnailUrl = thumbnailUrl;
 	}
 }
