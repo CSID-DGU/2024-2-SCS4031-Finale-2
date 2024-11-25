ba1537f1b6bfa850987f6d5e4304a28a155ee203
Yeonming <2019110914@dongguk.edu>
2024-11-25 16:54:08 +0900

    [feat] 감상평 작성 기능

---
+++ src/Backend/src/main/java/com/finale/review/service/ReviewService.java
@@ -0,0 +1,22 @@
+package com.finale.review.service;
+import com.finale.product.entity.Product;
+import com.finale.product.repository.ProductRepository;
+import com.finale.review.dto.ReviewRequest;
+import com.finale.review.repository.ReviewRepository;
+import com.finale.user.entity.User;
+import com.finale.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+    private final ProductRepository productRepository;
+    public void saveReview(ReviewRequest request, Long productId) {
+        User writer = userRepository.findById(request.writerId()).orElseThrow(() -> new IllegalArgumentException("유효하지 않은 writerID입니다."));
+        Product product = productRepository.findById(productId).orElseThrow(() -> new IllegalArgumentException("유효하지 않은 productID입니다."));
+        reviewRepository.save(request.toEntity(writer,product));
+    }
+ }
