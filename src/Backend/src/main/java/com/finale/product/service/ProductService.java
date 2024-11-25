139f04dd77045603698ef2b729af20d53346bd65
HeoJeeHae <hjh021124@dgu.ac.kr>
2024-11-25 21:58:26 +0900

    좋아요한 상품 조회 기능

--- src/Backend/src/main/java/com/finale/product/service/ProductService.java
+++ src/Backend/src/main/java/com/finale/product/service/ProductService.java
@@ -5,30 +5,48 @@ import com.finale.product.entity.Category;
 import com.finale.product.entity.Product;
 import com.finale.product.repository.ProductImageRepository;
 import com.finale.product.repository.ProductRepository;
-import com.finale.product.dto.ProductPage;
 import com.finale.user.entity.ArtistInfo;
 import com.finale.user.repository.ArtistInfoRepository;
+import com.finale.product.dto.ProductPage;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
     private final ProductImageRepository productImageRepository;
-    
+    private final ArtistInfoRepository artistInfoRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
+
     @Transactional(readOnly = true)
     public ProductPage.Paging getProductsByPage(String query, Pageable pageable) {
         var productPage = productRepository.findByNameWithIdx(query, pageable);
+
+        ZSetOperations<String, Object> zSet = redisTemplate.opsForZSet();
+        var zSetKey = "search:" + query;
+        var time = System.currentTimeMillis();
+        zSet.add(zSetKey, String.valueOf(time), time);
+
         return ProductPage.Paging.from(productPage);
     }
 
+    @Transactional(readOnly = true)
+    public ProductPage.Paging getProductsWithRandomPaging(Pageable pageable) {
+        var productPage = productRepository.findAllRandom(pageable);
+        return ProductPage.Paging.from(productPage);
+    }
 
     public Product save(ProductRequest productSaveRequest) {
-        //TODO ArtistInfo 코드 병합시 수정 예정
-        Product product = productSaveRequest.toEntity(null);
+        ArtistInfo artistInfo = artistInfoRepository.findById(productSaveRequest.artistInfoId())
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 작가 정보입니다."));
+        Product product = productSaveRequest.toEntity(artistInfo,productSaveRequest.getThumbnailImage());
         productRepository.save(product);
         return product;
     }
@@ -40,7 +58,8 @@ public class ProductService {
     @Transactional
     public void edit(Long productId, ProductRequest productRequest) {
         Product product = productRepository.findById(productId).orElseThrow(() -> new IllegalArgumentException("유효하지 않은 id입니다"));
-        //TODO ArtistInfo 코드 병합시 수정 예정
+        ArtistInfo artistInfo = artistInfoRepository.findById(productRequest.artistInfoId())
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 작가 정보입니다."));
         product.update(
                 productRequest.name(),
                 Category.fromString(productRequest.category()),
@@ -49,7 +68,13 @@ public class ProductService {
                 productRequest.description(),
                 productRequest.preferredLocation(),
                 productRequest.hashTags(),
-                null);
+                artistInfo);
+    }
+
+    @Transactional
+    public void editThumbnailImage(Long productId, List<String> images) {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new IllegalArgumentException("유효하지 않은 id입니다"));
+        product.updateThumbnail(images.getFirst());
     }
 
     public void delete(Long productId) {
