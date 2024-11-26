06c58d4f964e42c267a2585ea4878ffe3a71dda8
hanseo <yds06206@gmail.com>
2024-11-27 05:32:08 +0900

    feat: 오류 해결 및 페이지 컴포넌트 구현 완성

--- src/Frontend/vite-project/vite.config.ts
+++ src/Frontend/vite-project/vite.config.ts
@@ -1,23 +1,31 @@
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-      { find: '@/apis', replacement: path.resolve(__dirname, 'src/apis') },
-      { find: '@/assets', replacement: path.resolve(__dirname, 'src/assets') },
-      { find: '@/components', replacement: path.resolve(__dirname, 'src/components') },
-      { find: '@/hooks', replacement: path.resolve(__dirname, 'src/hooks') },
-      { find: '@/pages', replacement: path.resolve(__dirname, 'src/pages') },
-      { find: '@/routes', replacement: path.resolve(__dirname, 'src/routes') },
-      { find: '@/styles', replacement: path.resolve(__dirname, 'src/styles') },
-      { find: '@/types', replacement: path.resolve(__dirname, 'src/types') },
-      { find: '@/utils', replacement: path.resolve(__dirname, 'src/utils') },
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      { find: "@/apis", replacement: path.resolve(__dirname, "src/apis") },
+      { find: "@/assets", replacement: path.resolve(__dirname, "src/assets") },
+      {
+        find: "@/components",
+        replacement: path.resolve(__dirname, "src/components"),
+      },
+      { find: "@/hooks", replacement: path.resolve(__dirname, "src/hooks") },
+      {
+        find: "@/constants",
+        replacement: path.resolve(__dirname, "src/constants"),
+      },
+      { find: "@/pages", replacement: path.resolve(__dirname, "src/pages") },
+      { find: "@/routes", replacement: path.resolve(__dirname, "src/routes") },
+      { find: "@/styles", replacement: path.resolve(__dirname, "src/styles") },
+      { find: "@/store", replacement: path.resolve(__dirname, "src/store") },
+      { find: "@/types", replacement: path.resolve(__dirname, "src/types") },
+      { find: "@/utils", replacement: path.resolve(__dirname, "src/utils") },
     ],
   },
 });
