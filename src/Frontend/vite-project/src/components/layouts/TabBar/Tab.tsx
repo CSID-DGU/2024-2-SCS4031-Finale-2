c9ea8168c0a11f5ff42301f1ed486cd06846f61a
hanseo <yds06206@gmail.com>
2024-11-27 02:41:58 +0900

    Feat: Tab 컴포넌트 구현

--- src/Frontend/vite-project/src/components/layouts/TabBar/Tab.tsx
+++ src/Frontend/vite-project/src/components/layouts/TabBar/Tab.tsx
@@ -1,15 +1,19 @@
-import styled from '@emotion/styled';
-import { Link } from 'react-router-dom';
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
 
-interface TabProps {
-  label: string;
-  defaultIcon: React.ReactNode;
-  activeIcon: React.ReactNode;
-  linkTo: string;
-  isActive: boolean; // 현재 페이지인지 여부 / pathname === tab.linkTo
-}
+import type { Tab as TabType } from "@/constants/tabList";
 
-const Tab = ({ label, defaultIcon, activeIcon, linkTo, isActive }: TabProps) => {
+type TabProps = TabType & {
+  isActive: boolean;
+};
+
+const Tab = ({
+  label,
+  defaultIcon,
+  activeIcon,
+  linkTo,
+  isActive,
+}: TabProps) => {
   return (
     <Wrapper>
       <StyledLink isActive={isActive} to={linkTo}>
@@ -19,16 +23,13 @@ const Tab = ({ label, defaultIcon, activeIcon, linkTo, isActive }: TabProps) => 
     </Wrapper>
   );
 };
-
 export default Tab;
-
 const Wrapper = styled.div`
   display: flex;
   flex: 1;
 `;
-
 const StyledLink = styled(Link, {
-  shouldForwardProp: (prop) => prop !== 'isActive',
+  shouldForwardProp: (prop) => prop !== "isActive",
 })<{ isActive: boolean }>`
   display: flex;
   flex: 1;
@@ -40,12 +41,13 @@ const StyledLink = styled(Link, {
   font-size: var(--font-size-xxs);
   font-weight: 400;
   text-align: center;
-  color: ${({ isActive }) => (isActive ? 'var(--color-black)' : 'var(--color-gray-dk)')};
+  color: ${({ isActive }) =>
+    isActive ? "var(--color-black)" : "var(--color-gray-dk)"};
   transition: transform 0.2s ease;
   &:hover {
     color: var(--color-black);
     svg {
-      fill: var(--color-black);
+      color: var(--color-black);
     }
   }
   &:active {
