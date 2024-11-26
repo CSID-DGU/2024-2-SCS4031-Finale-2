5756b8525363f165a825cee63ea16b3356f24867
hanseo <yds06206@gmail.com>
2024-11-27 02:49:32 +0900

    Feat: List 데이터 상수 생성

---
+++ src/Frontend/vite-project/src/styles/HorizontalLine/index.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+const HorizontalLine = styled.hr`
+  width: 100%;
+  border: none;
+  height: 1px;
+  background-color: var(--color-gray-lt);
+`;
+
+export default HorizontalLine;
