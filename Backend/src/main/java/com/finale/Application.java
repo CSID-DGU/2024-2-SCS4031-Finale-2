419e337f4a278c64653894ebd115b7b7a9a9348f
Yeonming <2019110914@dongguk.edu>
2024-12-02 19:53:27 +0900

    오류수정

--- Backend/src/main/java/com/finale/Application.java
+++ Backend/src/main/java/com/finale/Application.java
@@ -1,7 +1,6 @@
 package com.finale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
